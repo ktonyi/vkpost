705243183f94cc78dd2974e9fbf3493a6d61f8b1
ktonyi <antonigorevich.k@gmail.com>
2021-02-27 19:01:01 +0300

    add v.2

---
+++ src/main/java/ru/netology/domain/ViewsInfo.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+public class ViewsInfo {
+
+    private int count;
+
+    public int getCount() {
+
+        return count;
+    }
+
+    public void setCount(int count) {
+
+        this.count = count;
+    }
+}
